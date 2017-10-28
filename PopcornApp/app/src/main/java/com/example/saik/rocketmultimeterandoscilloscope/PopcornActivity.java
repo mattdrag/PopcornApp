ddd703f9cf72a39f591f90b80e0e7f231e24714f
Matt Dragotto <matthewdragotto@gmail.com>
2017-10-27 17:22:51 -0700

    Pop states: - Added 3 states for controlling the flow of the recording - Changed some UI elements, including a pop state display in upper left corner - Modified timing so that graph more closely resembles real time TODO: - create algorithm for determining noise bounds - make UI more intuitive, such as not having pause functionality on record button because that will mess up timing

--- PopcornApp/app/src/main/java/com/example/saik/rocketmultimeterandoscilloscope/PopcornActivity.java
+++ PopcornApp/app/src/main/java/com/example/saik/rocketmultimeterandoscilloscope/PopcornActivity.java
@@ -31,7 +31,7 @@ public class PopcornActivity extends AppCompatActivity {
     private static String TAG = "Popcorn";
     private static final int RECORD_REQUEST_CODE = 101;
 
-    TextView display;
+    TextView display_txt, pop_phase_txt;
     boolean isRecording = false;
     Handler handler;
     private LineGraphSeries<DataPoint> series;
@@ -42,12 +42,11 @@ public class PopcornActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         handler = new Handler();
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_popcorn);
 
-        //Request microphone permission
+        // Request microphone permission
         int permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.RECORD_AUDIO);
 
@@ -56,9 +55,10 @@ public class PopcornActivity extends AppCompatActivity {
             makeRequest();
         }
 
-        display = (TextView) findViewById(R.id.voltmeter);
-        display.setText(new DecimalFormat("##.##").format(0) + " dbs");
-        final Button recTog = (Button) findViewById(R.id.ampmeterButton);
+        // Set up UI
+        display_txt = (TextView) findViewById(R.id.decibelMeter);
+        display_txt.setText(new DecimalFormat("##.##").format(0) + " dbs");
+        final Button recTog = (Button) findViewById(R.id.startButton);
         recTog.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (isRecording){
@@ -73,27 +73,100 @@ public class PopcornActivity extends AppCompatActivity {
                 }
             }
         });
+        pop_phase_txt = (TextView) findViewById(R.id.popPhase);
 
-        //TODO: Potentially 100ms is too much, we could try using Thread.sleep(5) for a 5ms delay but some other graphing code will have to change.
+        ////////////////////////////////////////
+        ////// Main thread for doing work //////
+        ////////////////////////////////////////
+
         final Runnable updater = new Runnable() {
+            //  Timing: 100-120ms for addEntry() call, sleeps for 10ms between calls.
+            boolean didStart = false;
+            long startTimeInMillis; // for switching states
+
+            //  States:
+            //  PRE_POP:    create ranges for ambient noise over 5,
+            //              changes state after 10s min of recording
+            //  POP_PHASE:  determine amplitude of pops,
+            //              begin recording interval between pops,
+            //              changes state when interval between pops is >= 2seconds
+            //  POST_POP:   Final state
+            //              triggers some alarm for user to notify them popcorn is done
+            byte POP_STATE = 0; //[ PRE_POP, POP_PHASE, POST_POP ] ==> [ 0, 1, 2 ]
+
+
+            //These bounds will be defined during the PRE_POP and POP_PHASE
+            //NOTE: Some threshold of noise will be too much for app to distinguish pops.
+            //      we predefine AMBIENT_MAX to be the maximum ambient
+            //      range the popcorn sensor can operate at
+            final int AMBIENT_MAX = 0; //TODO
+            int AMBIENT_NOISE_LOWER_BOUND = 0;
+            int AMBIENT_NOISE_UPPER_BOUND = 0;
+            int POPCORN_NOISE_LOWER_BOUND = 0;
+            int POPCORN_NOISE_UPPER_BOUND = 0;
+
             @Override
             public void run() {
-                if (isRecording) {
-                    display.setText(new DecimalFormat("##.##").format(audioMeter.getAmplitude()) + " dbs");
-                    addEntry();
-                    handler.postDelayed(this, 100); //30ms delay
+                if (isRecording) { //Start button has been pressed
+                    //Grab the current system time in milliseconds if we didn't already start yet
+                    if (!didStart){
+                        startTimeInMillis = System.currentTimeMillis();
+                        didStart = true;
+                    }
+
+                    //Do work depending on which phase we are in
+                    if (POP_STATE == 0){ //PRE_POP
+                        //TODO: For now, after 10 seconds, advance phase
+                        long deltaTime = System.currentTimeMillis() - startTimeInMillis;
+                        if (deltaTime >= 10000){
+                            POP_STATE = 1;
+                        }
+                        updateDisplay();
+                    }
+                    else if (POP_STATE == 1){ //POP_PHASE
+                        //TODO: For now, after 30 seconds, advance phase
+                        long deltaTime = System.currentTimeMillis() - startTimeInMillis;
+                        if (deltaTime >= 40000){
+                            POP_STATE = 2;
+                        }
+                        updateDisplay();
+                    }
+                    else{ //POST_POP
+                        updateDisplay();
+                    }
+                    handler.postDelayed(this, 10); //100ms delay
                 }
                 else {
-                    handler.postDelayed(this, 100); //30ms delay
+                    handler.postDelayed(this, 10); //100ms delay
                 }
+            }
+
+            //Updates all UI elements. helper for run() code
+            public void updateDisplay(){
+                display_txt.setText(new DecimalFormat("##.##").format(audioMeter.getAmplitude()) + " dbs");
+                if (POP_STATE == 0){ //PRE_POP
+                    pop_phase_txt.setText("PRE_POP");
+                }
+                else if (POP_STATE == 1){ //POP_PHASE
+                    pop_phase_txt.setText("POP_PHASE");
+                }
+                else{ //POST_POP
+                    pop_phase_txt.setText("POST_POP");
+                }
+                addEntry();  //Takes about 100-120ms
+                             //addEntry calls audioMeter.getAmplitude(), which is the bottleneck
             }
         };
         handler.post(updater);
 
+        ////////////////////////////////////////
+        ///// End of thread for doing work /////
+        ////////////////////////////////////////
+
 
         //setup graph
         // we get graph view instance
-        GraphView graph = (GraphView) findViewById(R.id.ampmeter);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
         // data
         series = new LineGraphSeries<>();
         graph.addSeries(series);
@@ -123,14 +196,14 @@ public class PopcornActivity extends AppCompatActivity {
         graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.WHITE);
     }
 
-    //request mic permission
+    // Function for requesting mic permission
     protected void makeRequest() {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.RECORD_AUDIO},
                 RECORD_REQUEST_CODE);
     }
 
-    //For graphing points
+    //Function for graphing points
     private void addEntry() {
         if (lastX < 10.0){
             series.appendData(new DataPoint(lastX, audioMeter.getAmplitude()), false, 10000); // we can store 10000 values at a time
@@ -138,7 +211,7 @@ public class PopcornActivity extends AppCompatActivity {
         else{
             series.appendData(new DataPoint(lastX, audioMeter.getAmplitude()), true, 10000); // we can store 10000 values at a time
         }
-        lastX+= 0.1;
+        lastX += 0.125; //Estimates delay to make timing on graph accurate to system time. Graph will roughly move along with real time, with some error.
     }
 
 
@@ -167,6 +240,9 @@ public class PopcornActivity extends AppCompatActivity {
             short[] buffer = new short[minSize];
             audioRecorder.read(buffer, 0, minSize);
             int max = 0;
+
+            //  Bottleneck for sampling rate. Buffer is 512 bytes, we are looping 512 times and finding
+            //  the max value.
             for (short s : buffer)
             {
                 if (Math.abs(s) > max)
@@ -174,6 +250,7 @@ public class PopcornActivity extends AppCompatActivity {
                     max = Math.abs(s);
                 }
             }
+
             return max;
         }
     }
